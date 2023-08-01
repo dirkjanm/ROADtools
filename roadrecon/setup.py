9dfea655e87ce9e2970b6be335c00dc0a7e9803b
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2023-08-01 14:36:37 +0200

    roadrecon: version increase for policies fix

--- roadrecon/setup.py
+++ roadrecon/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='roadrecon',
-      version='1.1.1',
+      version='1.1.2',
       description='Azure AD recon for red and blue',
       author='Dirk-jan Mollema',
       author_email='dirkjan@dirkjanm.io',
