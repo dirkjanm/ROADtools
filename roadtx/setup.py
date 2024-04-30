ec8f7f2eec1e5aea13808062c42a66a69648d6d7
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2024-04-30 19:23:09 +0200

    version increases

--- roadtx/setup.py
+++ roadtx/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='roadtx',
-      version='1.7.1',
+      version='1.8.0',
       description='ROADtools Token eXchange',
       author='Dirk-jan Mollema',
       author_email='dirkjan@outsidersecurity.nl',
@@ -18,7 +18,7 @@ setup(name='roadtx',
       packages=['roadtools.roadtx'],
       package_data={'roadtools.roadtx': ['firstpartyscopes.json']},
       install_requires=[
-          'roadlib>=0.23',
+          'roadlib>=0.24',
           'requests',
           'selenium',
           'selenium-wire',
