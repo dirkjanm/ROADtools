bbd706c579a332679b65acd91bd62ee6ef26e167
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2024-11-18 22:20:37 +0100

    pypi version increases

--- roadtx/setup.py
+++ roadtx/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='roadtx',
-      version='1.12.0',
+      version='1.12.1',
       description='ROADtools Token eXchange',
       author='Dirk-jan Mollema',
       author_email='dirkjan@outsidersecurity.nl',
@@ -18,7 +18,7 @@ setup(name='roadtx',
       packages=['roadtools.roadtx'],
       package_data={'roadtools.roadtx': ['firstpartyscopes.json']},
       install_requires=[
-          'roadlib>=0.27',
+          'roadlib>=0.29',
           'requests',
           'selenium',
           'selenium-wire',
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(name='roadtools',
-      version='0.0.1',
-      description='The Azure AD tooling framework',
+      version='0.0.2',
+      description=' A collection of Azure AD/Entra tools for offensive and defensive security purposes',
       author='Dirk-jan Mollema',
       author_email='dirkjan@dirkjanm.io',
       url='https://github.com/dirkjanm/ROADtools/',
@@ -13,8 +13,11 @@ setup(name='roadtools',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
       ],
       packages=[],
       zip_safe=False,
-      install_requires=['roadlib', 'roadrecon']
+      install_requires=['roadlib', 'roadrecon', 'roadtx']
       )
