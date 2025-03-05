c74e0998e3eb58b1b48613a51d1f4a1c96e81d35
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2025-03-05 14:21:12 +0100

    roadlib version

--- roadlib/setup.py
+++ roadlib/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_namespace_packages
 setup(name='roadlib',
-      version='1.2.0',
+      version='1.3.0',
       description='ROADtools common components library',
       author='Dirk-jan Mollema',
       author_email='dirkjan@outsidersecurity.nl',
